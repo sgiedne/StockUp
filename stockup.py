9e365ad0a9d8d7e5246daafd90eccfa962e4ba67
Siddharth <sgiedne@gmail.com>
2017-05-08 18:19:42 -0500

    First commit || Sidd || May 5 2017

---
+++ stockup.py
@@ -0,0 +1,16 @@
+import re
+import urllib2
+from bs4 import BeautifulSoup
+
+
+
+
+def symbolLookUp(company):
+	pattern = re.compile("\/quote\/.*\?")
+	raw_page = urllib2.urlopen("https://finance.yahoo.com/quote/" + company + "?p=" + company).read()
+	soup = BeautifulSoup(raw_page,"lxml")
+	for text in soup.findAll('a'):
+		if pattern.match(text):
+			print text
+
+symbolLookUp('facebook')
