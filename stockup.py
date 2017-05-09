c3e60caa039d187da008b18497057359556b640a
Siddharth <sgiedne@gmail.com>
2017-05-09 00:07:09 -0500

    Implemented tickerLookUp and parseStockTwits methods || Sidd || May 9 2017

--- stockup.py
+++ stockup.py
@@ -1,16 +1,23 @@
 import re
 import urllib2
 from bs4 import BeautifulSoup
-
-
-
+import json
+import requests
+import unicodedata
 
 def symbolLookUp(company):
-	pattern = re.compile("\/quote\/.*\?")
-	raw_page = urllib2.urlopen("https://finance.yahoo.com/quote/" + company + "?p=" + company).read()
-	soup = BeautifulSoup(raw_page,"lxml")
-	for text in soup.findAll('a'):
-		if pattern.match(text):
-			print text
+	url = "http://d.yimg.com/aq/autoc?query=" + company + "&region=IN&lang=en-UK&callback=YAHOO.Finance.SymbolSuggest.ssCallback"
+	raw_json = urllib2.urlopen(url).read()
+	res = json.loads(raw_json.split('YAHOO.Finance.SymbolSuggest.ssCallback(')[1].split(');')[0])
+	return res['ResultSet']['Query']
 
-symbolLookUp('facebook')
+def parseStockTwits(ticker):
+	url = "https://api.stocktwits.com/api/2/streams/symbol/" + ticker + ".json?filter=top"
+	raw_json = urllib2.urlopen(url).read()
+	res = json.loads(raw_json)
+	for msg in res['messages']:
+		print unicodedata.normalize('NFKD', msg['body']).encode('ascii','ignore')
+		print ''
+
+
+parseStockTwits('FB')
